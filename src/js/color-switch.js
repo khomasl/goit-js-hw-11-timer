009d22e97274a3aa8d6900ef4acdbe0d17633a7a
khomasl <gymnasium.yv@gmail.com>
2021-08-09 16:11:05 +0300

    changes

--- src/js/color-switch.js
+++
@@ -1,8 +0,0 @@
-import { btnStart, btnStop} from './refs.js';
-import { onColorSwitchStart, onColorSwitchStop } from './listeners.js';
-
-btnStart.addEventListener('click', onColorSwitchStart);
-btnStop.addEventListener('click', onColorSwitchStop);
-
-
-
